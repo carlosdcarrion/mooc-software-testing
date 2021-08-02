3bafdecf3a9af45d0dbd8d4f41a9146c852be244
carlosdcarrion <carlos.d.carrion@outlook.com>
2021-08-02 16:18:46 -0500

    Topic 2

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,14 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    @ParameterizedTest
+    @CsvSource({"abc,1,bcd","abc,2,cde","abc,3,def","abc,-1,zab","abc,-2,yza","abc,-3,xyz"})
+    public void cscTest (String message, int shift, String expectedResult) {
+        String result = new CaesarShiftCipher().CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,18 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "String={0}, Result={1}")
+    @CsvSource({"xxggxx,true",
+                "xxgxx,false",
+                "xxggyygxx,false",
+                "gxxxx,false"})
+    public void gHappyTest (String str,  boolean expectedResult) {
+        boolean result = new GHappy().gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
